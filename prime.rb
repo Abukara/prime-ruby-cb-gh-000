3a23289a36fb0958b457e4a5c43493ab140ccefa
Lukas F <lukas.forst48@googlemail.com>
2018-09-16 18:43:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(number)
+  i=1
+  if number< 0 || number == 0 || num==1
+    return false
+  else
+        (2..num-1).to_a.all? {|factor|num % possible_factor != 0 }
+  end
+end
+
+
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
+  end
+end
